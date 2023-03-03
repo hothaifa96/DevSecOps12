adc335f7c5061d97ae495a685b681d40c53b953d
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-03-03 14:04:17 +0200

    message

---
+++ 3-3/LambadaAndMap.py
@@ -0,0 +1,43 @@
+# fifo- first in first out
+# lifo- last in first out
+
+
+# list1 = ['1', 2, 5, 4]
+# list2 = list() # []
+#
+# list2.append(1) # adding
+# list2.remove() # value
+# list2.pop() # index
+# list1.sort()
+
+prices = [2, 5, 12, 4, 10, 100]
+
+# kaaar = lambda cash: 'fanta' if cash < 6 else 'fanta ' * (cash // 5)
+#
+# multi = lambda x, y: x * y
+#
+# print(multi(4, 5))
+# print(kaaar(20))
+
+# get_drink = lambda p : 'Coffee'*(p// 10)
+#
+# coffees =  map(get_drink,prices)
+#
+# print(list(coffees))
+
+# sq = lambda r : r**2
+#
+# numbers2 = list(map(sq,prices))
+
+# print(numbers2)
+
+# string = list(map(str,prices))
+# floats = list(map(float,prices))
+# print(prices)
+# print(string)
+# print(floats)
+
+# ['2','5','12',....]
+
+
+
