5f4d47369f63f362366f75d590672c1a5381474d
ofek.harapz <oharpaz1@gmail.com>
2023-03-28 19:55:39 +0300

    self

--- 17-3/random_demo.py
+++ 17-3/random_demo.py
@@ -1,15 +1,17 @@
-import random
+# import random
 
-# list1 = [['guess','aaaa'],'david','hohnatan','sari']
-#
-# print(random.choice(list1))
+# # list1 = [['guess','aaaa'],'david','hohnatan','sari']
+# #
+# # print(random.choice(list1))
 
-list2 = list(range(1, 11)) # [1,2,3,4,5,6,7,8,9]
-print(list2)
+# list2 = list(range(1, 11)) # [1,2,3,4,5,6,7,8,9]
+# print(list2)
 
-r1 = random.randint(0, len(list2))
-print(list2[r1])
+# r1 = random.randint(0, len(list2))
+# print(list2[r1])
 
-print(random.choice(list2))
+# print(random.choice(list2))
 
 
+# typys = (input("bla bla bla"))
+# print(bin(typys))
