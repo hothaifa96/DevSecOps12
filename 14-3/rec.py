25e36b6e237e2237355a155b889c7446a1f2bb85
hothaifa zoubi <hothaifazoubi@gmail.com>
2023-05-05 09:54:51 +0300

    DockerInit

--- 14-3/rec.py
+++
@@ -1,9 +0,0 @@
-def demo(counter):
-    if counter == 10:
-        return None
-    print(counter)
-    counter += 1
-    demo(counter)
-
-
-demo(0)
--- 4-4/functions.py
+++
@@ -1,9 +0,0 @@
-def greet():
-    print('hello')
-
-
-def greet_with_name(name):
-    print('hello', name)
-
-
-s1 = 'victor'
